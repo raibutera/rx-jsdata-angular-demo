5db44a5e47bfe6473760d75be9e035c49c870404
Raimondo Butera <rai@rbutera.com>
2015-05-28 12:47:28 +0100

    injected dependencies

--- src/scripts/common/index.js
+++ src/scripts/common/index.js
@@ -1,6 +1,17 @@
 'use strict';
 require('angular-ui-router');
 
+var Bluebird = require('bluebird');
+require('moment');
+var _ = require('lodash');
+var faker = require('faker');
+var Rx = require('rx');
+require('angular-rx');
+require('localforage');
+require('angular-localforage');
+require('js-data');
+require('js-data-localforage');
+
 var modulename = 'common';
 
 module.exports = function(namespace) {
@@ -8,9 +19,14 @@ module.exports = function(namespace) {
     var fullname = namespace + '.' + modulename;
 
     var angular = require('angular');
-    var app = angular.module(fullname, ['ui.router', ]);
+    var app = angular.module(fullname, ['ui.router', 'LocalForageModule', 'js-data', 'rx']);
     // inject:folders start
     // inject:folders end
+
+    app.constant('lodash', _);
+    app.constant('faker', faker);
+    app.constant('moment', moment);
+    app.constant('Bluebird', Bluebird);
 
     app.config(['$stateProvider', '$urlRouterProvider',
         function($stateProvider, $urlRouterProvider) {
